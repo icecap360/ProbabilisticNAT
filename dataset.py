4d6c247243ad50afbbd0759fdf5021dc75492873
icecap360 <muhammad.qasim.ali06@gmail.com>
2025-01-20 12:56:25 -0500

    Fixed gibbs sampler bug

--- dataset.py
+++ dataset.py
@@ -1,15 +1,16 @@
+import math
+import os
+import random
 from pathlib import Path
 
-from torch.utils.data import Dataset
 import numpy as np
 import torch
-import math
-import random
-from PIL import Image
-import os
 from loguru import logger
+from PIL import Image
+from torch.utils.data import Dataset
 
 print = logger.info
+
 
 class CFGDataset(Dataset):  # for classifier free guidance
     def __init__(self, dataset, p_uncond, empty_token):
@@ -48,7 +49,7 @@ class DatasetFactory(object):
             return dataset
 
     def unpreprocess(self, v):  # to B C H W and [0, 1]
-        v.clamp_(0., 1.)
+        v.clamp_(0.0, 1.0)
         return v
 
     @property
@@ -73,62 +74,74 @@ class DatasetFactory(object):
     def label_prob(self, k):
         raise NotImplementedError
 
+
 class FeatureDataset(Dataset):
     def __init__(self, path, split, n_extras):
         super().__init__()
         self.path = path
         self.split = split
         self.n_extras = n_extras
-        data_path = f'{path}/{split}.npy'
+        data_path = f"{path}/{split}.npy"
         self.data = np.load(data_path)
 
     def __len__(self):
         return len(self.data)  # consider the random flip
 
     def __getitem__(self, idx):
-        return self.data[idx][self.n_extras:], self.data[idx][0:self.n_extras]
+        return self.data[idx][self.n_extras :], self.data[idx][0 : self.n_extras]
 
 
-class ImageNet256Features(DatasetFactory):  # the moments calculated by Stable Diffusion image encoder
+class ImageNet256Features(
+    DatasetFactory
+):  # the moments calculated by Stable Diffusion image encoder
     def __init__(self, path, cfg=False, p_uncond=None):
         super().__init__()
-        print('Prepare dataset...')
-        self.train = FeatureDataset(path, split='train', n_extras=1)
-        print('Prepare dataset ok')
+        print("Prepare dataset...")
+        self.train = FeatureDataset(path, split="train", n_extras=1)
+        print("Prepare dataset ok")
         self.K = 1000
         self.empty_context = np.array([self.K], dtype=np.longlong)
 
         if cfg:  # classifier free guidance
             assert p_uncond is not None
-            print(f'prepare the dataset for classifier free guidance with p_uncond={p_uncond}')
+            print(
+                f"prepare the dataset for classifier free guidance with p_uncond={p_uncond}"
+            )
             self.train = CFGDataset(self.train, p_uncond, self.empty_context)
 
 
 class ImageNet(DatasetFactory):
-    def __init__(self, path, resolution, random_crop=False, random_flip=True, split='train'):
+    def __init__(
+        self, path, resolution, random_crop=False, random_flip=True, split="train"
+    ):
         super().__init__()
 
-        print(f'Counting ImageNet files from {path}')
+        print(f"Counting ImageNet files from {path}")
         train_files = _list_image_files_recursively(os.path.join(path, split))
         # class_names = [os.path.basename(path).split("_")[0] for path in train_files]
         class_names = [Path(path).parent.name for path in train_files]
         sorted_classes = {x: i for i, x in enumerate(sorted(set(class_names)))}
         train_labels = [sorted_classes[x] for x in class_names]
-        print('Finish counting ImageNet files')
+        print("Finish counting ImageNet files")
 
-        self.train = ImageDataset(resolution, train_files, labels=train_labels, random_crop=random_crop,
-                                  random_flip=random_flip)
+        self.train = ImageDataset(
+            resolution,
+            train_files,
+            labels=train_labels,
+            random_crop=random_crop,
+            random_flip=random_flip,
+        )
         self.resolution = resolution
         if len(self.train) != 1_281_167:
-            print(f'Missing train samples: {len(self.train)} < 1281167')
+            print(f"Missing train samples: {len(self.train)} < 1281167")
 
         self.K = max(self.train.labels) + 1
         cnt = dict(zip(*np.unique(self.train.labels, return_counts=True)))
-        self.cnt = torch.tensor([cnt[k] for k in range(self.K)]).float()
+        self.cnt = torch.tensor([int(cnt[k]) for k in range(self.K)]).float()
         self.frac = [self.cnt[k] / len(self.train.labels) for k in range(self.K)]
-        print(f'{self.K} classes')
-        print(f'cnt[:10]: {self.cnt[:10]}')
-        print(f'frac[:10]: {self.frac[:10]}')
+        print(f"{self.K} classes")
+        print(f"cnt[:10]: {self.cnt[:10]}")
+        print(f"frac[:10]: {self.frac[:10]}")
 
 
 def _list_image_files_recursively(data_dir):
@@ -145,12 +158,12 @@ def _list_image_files_recursively(data_dir):
 
 class ImageDataset(Dataset):
     def __init__(
-            self,
-            resolution,
-            image_paths,
-            labels,
-            random_crop=False,
-            random_flip=True,
+        self,
+        resolution,
+        image_paths,
+        labels,
+        random_crop=False,
+        random_flip=True,
     ):
         super().__init__()
         self.resolution = resolution
@@ -172,6 +185,7 @@ class ImageDataset(Dataset):
             except OSError as e:
                 print(f"Catched exception: {str(e)}. Re-trying...")
                 import time
+
                 time.sleep(1)
             else:
                 break
@@ -184,10 +198,10 @@ class ImageDataset(Dataset):
         if self.random_flip and random.random() < 0.5:
             arr = arr[:, ::-1]
 
-        arr = arr.astype(np.float32) / 255.
+        arr = arr.astype(np.float32) / 255.0
 
-        label = np.array(self.labels[idx], dtype=np.int64)
-        return np.transpose(arr, [2, 0, 1]), label
+        label = np.array(self.labels[idx], dtype=np.int32)
+        return np.transpose(arr, [2, 0, 1]).astype(np.float32), label
 
 
 def center_crop_arr(pil_image, image_size):
@@ -207,7 +221,7 @@ def center_crop_arr(pil_image, image_size):
     arr = np.array(pil_image)
     crop_y = (arr.shape[0] - image_size) // 2
     crop_x = (arr.shape[1] - image_size) // 2
-    return arr[crop_y: crop_y + image_size, crop_x: crop_x + image_size]
+    return arr[crop_y : crop_y + image_size, crop_x : crop_x + image_size]
 
 
 def random_crop_arr(pil_image, image_size, min_crop_frac=0.8, max_crop_frac=1.0):
@@ -231,16 +245,18 @@ def random_crop_arr(pil_image, image_size, min_crop_frac=0.8, max_crop_frac=1.0)
     arr = np.array(pil_image)
     crop_y = random.randrange(arr.shape[0] - image_size + 1)
     crop_x = random.randrange(arr.shape[1] - image_size + 1)
-    return arr[crop_y: crop_y + image_size, crop_x: crop_x + image_size]
+    return arr[crop_y : crop_y + image_size, crop_x : crop_x + image_size]
 
 
 def center_crop(width, height, img):
-    resample = {'box': Image.BOX, 'lanczos': Image.LANCZOS}['lanczos']
+    resample = {"box": Image.BOX, "lanczos": Image.LANCZOS}["lanczos"]
     crop = np.min(img.shape[:2])
-    img = img[(img.shape[0] - crop) // 2: (img.shape[0] + crop) // 2,
-          (img.shape[1] - crop) // 2: (img.shape[1] + crop) // 2]
+    img = img[
+        (img.shape[0] - crop) // 2 : (img.shape[0] + crop) // 2,
+        (img.shape[1] - crop) // 2 : (img.shape[1] + crop) // 2,
+    ]
     try:
-        img = Image.fromarray(img, 'RGB')
+        img = Image.fromarray(img, "RGB")
     except:
         img = Image.fromarray(img)
     img = img.resize((width, height), resample)
@@ -249,11 +265,9 @@ def center_crop(width, height, img):
 
 
 def get_dataset(name, **kwargs):
-    if name == 'imagenet':
+    if name == "imagenet":
         return ImageNet(**kwargs)
-    elif name == 'imagenet256_features':
+    elif name == "imagenet256_features":
         return ImageNet256Features(**kwargs)
     else:
         raise NotImplementedError(name)
-
-
--- extract_imagenet_feature.py
+++ extract_imagenet_feature.py
@@ -1,23 +1,35 @@
+import argparse
 import os
-import torch.nn as nn
+
 import numpy as np
 import torch
-from dataset import ImageNet
+import torch.nn as nn
 from torch.utils.data import DataLoader
-import argparse
 from tqdm import tqdm
+
+from dataset import ImageNet
 
 torch.manual_seed(0)
 np.random.seed(0)
 
 
-def main(resolution=256, split='train'):
-    dataset = ImageNet(path=args.path, resolution=resolution, random_flip=False, split=split).train
-    dataset_loader = DataLoader(dataset, batch_size=128, shuffle=False, drop_last=False,
-                                num_workers=8, pin_memory=True, persistent_workers=True)
+def main(resolution=256, split="train"):
+    dataset = ImageNet(
+        path=args.path, resolution=resolution, random_flip=False, split=split
+    ).train
+    dataset_loader = DataLoader(
+        dataset,
+        batch_size=128,
+        shuffle=False,
+        drop_last=False,
+        num_workers=8,
+        pin_memory=True,
+        persistent_workers=True,
+    )
 
     import taming.models.vqgan
-    model = taming.models.vqgan.get_model('vq-f16-jax.yaml')
+
+    model = taming.models.vqgan.get_model("vq-f16-jax.yaml")
 
     model = nn.DataParallel(model)
     model.eval()
@@ -30,23 +42,26 @@ def main(resolution=256, split='train'):
         for batch in tqdm(dataset_loader):
             img, label = batch
             img = img.to(device)
-            if split == 'train':
+            if split == "train":
                 img = torch.cat([img, img.flip(dims=[-1])], dim=0)
                 label = torch.cat([label, label], dim=0)
             label = label.detach().cpu().numpy()
             N = len(label)
             batch = model(img)
             feat = batch[-1][-1].detach().cpu().numpy()
-            feat_all.append(np.concatenate((label[:, None], feat.reshape(N, -1)), axis=-1))
+            feat_all.append(
+                np.concatenate((label[:, None], feat.reshape(N, -1)), axis=-1)
+            )
     feat_all = np.concatenate(feat_all)
 
-    out_dir = f'assets/imagenet{resolution}_vq_features'
+    out_dir = f"/pub3/qasim/probnat/imagenet{resolution}_vq_features"
     os.makedirs(out_dir, exist_ok=True)
-    np.save(f'{out_dir}/{split}.npy', feat_all)
+    np.save(f"{out_dir}/{split}.npy", feat_all)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--path', type=str)
-    parser.add_argument('--split', type=str, default='train')
+    parser.add_argument("--path", type=str, default="/pub1/data/ImageNet/")
+    parser.add_argument("--split", type=str, default="train")
     args = parser.parse_args()
     main(resolution=256, split=args.split)
